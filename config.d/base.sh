266a1b020a3e0bdf6411abe7cc464b689fad29c5
Masahito Yoshida <masahito@axsh.net>
2015-01-16 15:33:39 +0900

    setup dotfiles.

--- config.d/base.sh
+++ config.d/base.sh
@@ -11,6 +11,14 @@ set -x
 
 user=${user:-vagrant}
 
+# dotfiles
+
+su - ${user} -c "bash -ex" <<'EOS'
+  curl -fSkL https://raw.githubusercontent.com/hansode/env-bootstrap/master/build-personal-env.sh | bash
+EOS
+
+# rbenv
+
 su - ${user} -c "bash -ex" <<'EOS'
   if ! [[ -d ${HOME}/.rbenv ]]; then
     git clone https://github.com/sstephenson/rbenv.git ${HOME}/.rbenv
@@ -29,6 +37,8 @@ su - ${user} -c "bash -ex" <<'EOS'
   fi
 EOS
 
+# ruby
+
 su - ${user} -c "bash -ex" <<'EOS'
   version=2.2.0
 
@@ -46,6 +56,8 @@ su - ${user} -c "bash -ex" <<'EOS'
   rbenv exec gem install bundler
   rbenv exec gem list
 EOS
+
+# retrobot
 
 su - ${user} -c "bash -ex" <<'EOS'
   if ! [[ -d retrobot ]]; then
